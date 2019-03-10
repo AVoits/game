deab771c874ec80d2952631131e0b91b496ee34c
Anastasiya Voitsenya <rufousrat@gmail.com>
2019-03-10 20:41:11 +0300

    Updates

---
+++ precache-manifest.e36b612af9aaa8ad94e4d35ff68302e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ce91bc3c79b41efd61a",
+    "url": "/game/static/js/runtime~main.ce8c7c9e.js"
+  },
+  {
+    "revision": "03af3d2ff96d1f46e110",
+    "url": "/game/static/js/main.3c1782b3.chunk.js"
+  },
+  {
+    "revision": "0a4b832e1ec68f6c73d8",
+    "url": "/game/static/js/2.f44f96ce.chunk.js"
+  },
+  {
+    "revision": "03af3d2ff96d1f46e110",
+    "url": "/game/static/css/main.db0edf05.chunk.css"
+  },
+  {
+    "revision": "94a6d5ac1fb364c5fed8cca2c91b9dc7",
+    "url": "/game/index.html"
+  }
+];
