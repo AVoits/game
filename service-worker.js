794c1e9e836c269c4dd9f5d494a4322fc00c8f9a
Anastasiya Voitsenya <rufousrat@gmail.com>
2019-03-10 22:44:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/game/precache-manifest.609354b64350316a4c454c4dee8095d7.js"
+  "/game/precache-manifest.d637db268f714bf497ec10fa6d7e6195.js"
 );
 
 workbox.clientsClaim();
