ce09ff0681b29a6072622ae7a461fef33cf74db6
Anastasiya Voitsenya <rufousrat@gmail.com>
2019-03-10 22:58:44 +0300

    Updates

--- precache-manifest.d637db268f714bf497ec10fa6d7e6195.js
+++ precache-manifest.d637db268f714bf497ec10fa6d7e6195.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/game/static/js/runtime~main.ce8c7c9e.js"
   },
   {
-    "revision": "940e5208c9fff2754a71",
-    "url": "/game/static/js/main.c3af1104.chunk.js"
+    "revision": "9b5b0a3f626d53a03325",
+    "url": "/game/static/js/main.2a4a0828.chunk.js"
   },
   {
     "revision": "0a4b832e1ec68f6c73d8",
     "url": "/game/static/js/2.f44f96ce.chunk.js"
   },
   {
-    "revision": "940e5208c9fff2754a71",
+    "revision": "9b5b0a3f626d53a03325",
     "url": "/game/static/css/main.db0edf05.chunk.css"
   },
   {
-    "revision": "1f8fe7a6481999b106a1cf42c9c561fc",
+    "revision": "19def1317829e14e3f460abc32bebf93",
     "url": "/game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/game/precache-manifest.323788cf40444d3859a48e83ad590ff7.js"
+  "/game/precache-manifest.d637db268f714bf497ec10fa6d7e6195.js"
 );
 
 workbox.clientsClaim();
